0d784f271aa21c98c447a36a6e91c612038e465a
moe kandi <moe.kandi@socialsecurity.gov.scot>
2025-03-06 12:36:05 +0000

    removed ignore

---
+++ deployment/parameters.sh
@@ -0,0 +1,29 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://d-9c676e6f5d.awsapps.com/start
+REGION=eu-west-2
+TEAM_ACCOUNT=626635440741
+ORG_MASTER_PROFILE=moe-root-profile
+TEAM_ACCOUNT_PROFILE=moe-root2-profile
+TEAM_ADMIN_GROUP="TEAM admins"
+TEAM_AUDITOR_GROUP="TEAM auditor"
+TAGS="project=iam-identity-center-team environment=prod"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:eu-west-2:626635440741:eventdatastore/2a658d61-3d09-47e7-8390-1e5dd3c6ffd7
+SECRET_NAME=aws-team/
+
+# Uncomment the next line only if you have a custom domain
+# UI_DOMAIN=portal.teamtest.online
+
+
