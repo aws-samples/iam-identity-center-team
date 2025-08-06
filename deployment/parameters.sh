920439245e7fc55e96b6adb724405ccf95bb0529
MayurArsule <150061214+MayurArsule@users.noreply.github.com>
2025-08-06 08:32:05 +0530

    Create parameters.sh

---
+++ deployment/parameters.sh
@@ -0,0 +1,11 @@
+IDC_LOGIN_URL=https://d-9267f6d197.awsapps.com/start
+REGION=us-west-2
+ORG_MASTER_PROFILE=org_member_profile
+TEAM_ACCOUNT_PROFILE=team_account_profile
+TEAM_ADMIN_GROUP="Team-Admin"
+TEAM_AUDITOR_GROUP="Team-Auditor"
+TAGS="project=iam-identity-center-team environment=prod"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:us-west-2:739275461895:trail/management-events
+GITHUB_OWNER=MayurArsule
+GITHUB_REPO=IAM-TEAM-POC
+GITHUB_OAUTH_TOKEN=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
